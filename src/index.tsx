cdc0f1a2afa23be569267afe7f0cc36cc067b232
ale <ale@ale.com>
2022-08-08 17:54:01 -0300

    feat: add svgs and created files to routes, pages and server

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
-import { theme } from "./style/theme";
+
+import { AppProvider } from "./contexts";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root") as HTMLElement
 );
